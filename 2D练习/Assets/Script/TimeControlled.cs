afea30de357609eb1c065e56817a5d14aca0791b
haisen10086 <17370799105@163.com>
2026-04-21 14:53:14 +0800

    添加一个2d练习

---
+++ 2D练习/Assets/Script/TimeControlled.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeControlled : MonoBehaviour
+{
+    public Vector2 velocity;
+
+    public virtual void TimeUpdate()
+    {
+        
+    }
+}
